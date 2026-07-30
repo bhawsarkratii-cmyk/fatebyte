1d16928d730d2b9f22ad376236e44f1fd24cc690
bhawsarkratii-cmyk <bhawsarkratii@gmail.com>
2026-07-30 10:17:18 +0530

    Add files via upload

---
+++ fortune.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(__name__)
+
+fortunes = [
+    "You are going into your dream college! 😍",
+    "You will meet someone special today 😊",
+    "Something exciting is coming your way! ✨",
+    "Good things take time. Keep going! 🌸",
+    "Your hard work will pay off! 💫"
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/fortune")
+def fortune():
+    return jsonify({
+        "fortune": random.choice(fortunes)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
