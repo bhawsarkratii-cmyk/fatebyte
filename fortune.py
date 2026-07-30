23176df89d68feb817522bb3d427cf821fd71d2e
bhawsarkratii-cmyk <bhawsarkratii@gmail.com>
2026-07-30 11:42:07 +0530

    Add new fortunes to fortune.py

--- fortune.py
+++ fortune.py
@@ -4,12 +4,21 @@ import random
 app = Flask(__name__)
 
 fortunes = [
+    "Too glam to give a damn today. 💋✨",
     "You are going into your dream college! 😍",
+    "Keep shining; it is blinding your haters. 😎✨",
+    "Less perfection, way more authentic, gorgeous energy. 🔥💅",
+    "Your vibe is expensive; act like it. 💎💅",
+    "Manifesting miracles and massive checks for you. 💸✨",
     "You will meet someone special today 😊",
     "Something exciting is coming your way! ✨",
     "Good things take time. Keep going! 🌸",
-    "Your hard work will pay off! 💫"
-    "you will eat your favourite cheescake today"
+    "Your hard work will pay off! 💫",
+    "you will eat your favourite cheescake today",
+    "Standardize your crown; you are ruling today. 👑✨",
+    "You are the plot twist they needed. 😉🌟",
+
+    
 ]
 
 @app.route("/")
